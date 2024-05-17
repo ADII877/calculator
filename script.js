69d03ae740ee54f15222c117a1996df2be4c3253
ADII877 <155642627+ADII877@users.noreply.github.com>
2024-05-17 15:21:45 +0530

    add my calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value = display.value + number;
+}
+
+function appendOperator(operator) {
+    const display = document.getElementById('display');
+    display.value = display.value + ' ' + operator + ' ';
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
